eb6f1e198bab6014d3326064f4718260d28b1206
Kurt Sab <sabqkurt@gmail.com>
2026-02-26 10:52:19 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+
+const URL = "https://bit.ly/ncmhonlinekonsulta";
+
+async function checkWebsite() {
+  try {
+    const response = await fetch(URL);
+    const html = await response.text();
+
+    const closedText = "This form is no longer accepting responses";
+
+    if (!html.includes(CLOSED_TEXT)) {
+      console.log("Form is OPEN!");
+      await sendTelegramMessage("🚨 The form is now OPEN!");
+      process.exit(10);
+    } else {
+      console.log("Form still closed.");
+      process.exit(0);
+    }
+
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
+  }
+}
+
+async function sendTelegramMessage(message) {
+  const token = process.env.TELEGRAM_TOKEN;
+  const chatId = process.env.CHAT_ID;
+
+  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message
+    })
+  });
+
+}
+
+checkWebsite();
